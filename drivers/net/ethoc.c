988a75f0c0043060eb006e9a4bd93e1c1cde6742
Henrik Nordstrom <henrik@henriknordstrom.net>
2013-04-22 14:39:13 +0200

    ethod: Remove obsolete PRIVATE ioctl for getting hardware MAC address. Automatically mirrored to active MAC when needed.

--- drivers/net/ethoc.c
+++ drivers/net/ethoc.c
@@ -837,11 +837,6 @@ static int ethoc_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 		return -EINVAL;
 	}
 
-
-	if (cmd == SIOCDEVPRIVATE) {
-	   return ethoc_get_mac_address(dev, dev->dev_addr);
-	}
-
 	if (!priv->mdio)
 		return -EINVAL;
 
